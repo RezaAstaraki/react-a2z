9c8dfb19d2a82c29c54de0dbab13e8f4448fdff4
RezaAstarki <reza.astaraky@gmail.com>
2025-02-05 17:15:39 +0330

    fixed but type index issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,32 @@
-import resolve from "@rollup/plugin-node-resolve"
-import commonjs from "@rollup/plugin-commonjs"
-import typescript from "@rollup/plugin-typescript"
-import dts from "rollup-plugin-dts"
-
-const packageJson = require("./package.json");
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
 export default [
-    {
-        input: 'src/index.ts',
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            }
-        ],
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: './tsconfig.json' })
-        ]
-    },
-    {
-        input: 'dist/esm/types/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-        plugins: [dts()],
-    }
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "./dist/cjs/index.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "./dist/esm/index.js",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json", declaration: false }),
+    ],
+  },
+  //   {
+  //     input: "./dist/types/src/index.d.ts",
+  //     output: [{ file: "dist/esm/index.d.ts", format: "esm" }],
+  //     plugins: [dts()],
+  //   },
 ];
